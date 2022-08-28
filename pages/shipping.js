722be5da69078adca62eab698be314f1c90ffc9d
Arifur Rahman <arif037@pop-os.localdomain>
2022-08-28 14:34:27 +0600

    checkout screen created

---
+++ pages/shipping.js
@@ -0,0 +1,160 @@
+import Cookies from 'js-cookie';
+import { useRouter } from 'next/router';
+import React, { useContext, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import CheckoutWizard from '../components/CheckoutWizard';
+import Layout from '../components/Layout';
+import { Store } from '../utils/Store';
+
+const ShippingScreen = () => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    setValue,
+  } = useForm();
+
+  const { state, dispatch } = useContext(Store);
+  const { cart } = state;
+  const { shippingAddress } = cart;
+  const router = useRouter();
+
+  useEffect(() => {
+    setValue('fullName', shippingAddress.fullName);
+    setValue('phone', shippingAddress.phone);
+    setValue('address', shippingAddress.address);
+    setValue('city', shippingAddress.city);
+    setValue('postalCode', shippingAddress.postalCode);
+    setValue('country', shippingAddress.country);
+  }, [setValue, shippingAddress]);
+
+  const submitHandler = ({
+    fullName,
+    phone,
+    address,
+    city,
+    postalCode,
+    country,
+  }) => {
+    dispatch({
+      type: 'SAVE_SHIPPING_ADDRESS',
+      payload: { fullName, phone, address, city, postalCode, country },
+    });
+    Cookies.set(
+      'cart',
+      JSON.stringify({
+        ...cart,
+        shippingAddress: {
+          fullName,
+          phone,
+          address,
+          city,
+          postalCode,
+          country,
+        },
+      })
+    );
+    router.push('/payment');
+  };
+
+  return (
+    <Layout title="Shipping Address">
+      <CheckoutWizard activeStep={1}></CheckoutWizard>
+      <form
+        className="mx-auto max-w-screen-md"
+        onSubmit={handleSubmit(submitHandler)}
+      >
+        <h1 className="mb-4 text-xl">Shipping Address</h1>
+        <div className="mb-4">
+          <label htmlFor="fullName">Full Name</label>
+          <input
+            type="text"
+            className="w-full"
+            id="fullName"
+            autoFocus
+            {...register('fullName', { required: 'Please enter full name' })}
+          />
+          {errors.fullName && (
+            <div className="text-red-500">{errors.fullName.message}</div>
+          )}
+        </div>
+        <div className="mb-4">
+          <label htmlFor="phone">Phone</label>
+          <input
+            type="tel"
+            className="w-full"
+            id="phone"
+            {...register('phone', {
+              required: 'Please enter a valid phone number',
+            })}
+          />
+          {errors.phone && (
+            <div className="text-red-500">{errors.phone.message}</div>
+          )}
+        </div>
+        <div className="mb-4">
+          <label htmlFor="address">Address</label>
+          <input
+            type="text"
+            className="w-full"
+            id="address"
+            {...register('address', {
+              required: 'Please enter your address',
+              minLength: {
+                value: 3,
+                message: 'Address is more than 2 characters',
+              },
+            })}
+          />
+          {errors.address && (
+            <div className="text-red-500">{errors.address.message}</div>
+          )}
+        </div>
+        <div className="mb-4">
+          <label htmlFor="city">City</label>
+          <input
+            type="text"
+            className="w-full"
+            id="city"
+            {...register('city', { required: 'Please enter your city' })}
+          />
+          {errors.city && (
+            <div className="text-red-500">{errors.city.message}</div>
+          )}
+        </div>
+        <div className="mb-4">
+          <label htmlFor="postalCode">Postal Code</label>
+          <input
+            type="text"
+            className="w-full"
+            id="postalCode"
+            {...register('postalCode', {
+              required: 'Please enter postal code',
+            })}
+          />
+          {errors.postalCode && (
+            <div className="text-red-500">{errors.postalCode.message}</div>
+          )}
+        </div>
+        <div className="mb-4">
+          <label htmlFor="country">Country</label>
+          <input
+            type="text"
+            className="w-full"
+            id="country"
+            {...register('country', { required: 'Please enter your country' })}
+          />
+          {errors.country && (
+            <div className="text-red-500">{errors.country.message}</div>
+          )}
+        </div>
+        <div className="mb-4 flex justify-between">
+          <button className="primary-button">Next</button>
+        </div>
+      </form>
+    </Layout>
+  );
+};
+
+export default ShippingScreen;
+ShippingScreen.auth = true;
--- utils/Store.js
+++ utils/Store.js
@@ -6,7 +6,7 @@ export const Store = createContext();
 const initialState = {
   cart: Cookies.get('cart')
     ? JSON.parse(Cookies.get('cart'))
-    : { cartItems: [] },
+    : { cartItems: [], shippingAddress: {} },
 };
 
 const reducer = (state, action) => {
@@ -40,6 +40,18 @@ const reducer = (state, action) => {
           cartItems: [],
           shippingAddress: { location: {} },
           paymentMethod: '',
+        },
+      };
+    }
+    case 'SAVE_SHIPPING_ADDRESS': {
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          shippingAddress: {
+            ...state.cart.shippingAddress,
+            ...action.payload,
+          },
         },
       };
     }
