b9bf96f695ee7248a08597b74aaa332948885d0b
Arifur Rahman <arif037@pop-os.localdomain>
2022-08-30 22:06:01 +0600

    registration page and api created

--- pages/register.js
+++ pages/register.js
@@ -6,6 +6,7 @@ import Layout from '../components/Layout';
 import { getError } from '../utils/error';
 import toast from 'react-hot-toast';
 import { useRouter } from 'next/router';
+import axios from 'axios';
 
 const LoginScreen = () => {
   const { data: session } = useSession();
@@ -22,10 +23,17 @@ const LoginScreen = () => {
   const {
     handleSubmit,
     register,
+    getValues,
     formState: { errors },
   } = useForm();
-  const submitHandler = async ({ email, password }) => {
+  const submitHandler = async ({ name, email, password }) => {
     try {
+      await axios.post('/api/auth/signup', {
+        name,
+        email,
+        password,
+      });
+
       const result = await signIn('credentials', {
         redirect: false,
         email,
@@ -41,12 +49,26 @@ const LoginScreen = () => {
   };
 
   return (
-    <Layout title="Login">
+    <Layout title="Create Account">
       <form
         className="mx-auto max-w-screen-md"
         onSubmit={handleSubmit(submitHandler)}
       >
-        <h1 className="mb-4 text-xl">Login</h1>
+        <h1 className="mb-4 text-xl">Create Account</h1>
+        <div className="mb-4">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            className="w-full"
+            id="name"
+            autoFocus
+            {...register('name', { required: 'Please enter your name' })}
+          />
+          {errors.name && (
+            <div className="text-red-500">{errors.name.message}</div>
+          )}
+        </div>
+
         <div className="mb-4">
           <label htmlFor="email">Email</label>
           <input
@@ -60,7 +82,6 @@ const LoginScreen = () => {
             })}
             id="email"
             className="w-full"
-            autoFocus
           />
           {errors.email && (
             <div className="text-red-500">{errors.email.message}</div>
@@ -84,13 +105,34 @@ const LoginScreen = () => {
             <div className="text-red-500">{errors.password.message}</div>
           )}
         </div>
+
         <div className="mb-4">
-          <button className="primary-button">Login</button>
+          <label htmlFor="confirm-password">Confirm Password</label>
+          <input
+            type="password"
+            id="confirm-password"
+            className="w-full"
+            {...register('confirmPassword', {
+              required: 'Please confirm your password',
+              validate: (value) => value === getValues('password'),
+            })}
+          />
+          {errors.confirmPassword && (
+            <div className="text-red-500">{errors.confirmPassword.message}</div>
+          )}
+          {errors.confirmPassword &&
+            errors.confirmPassword.type === 'validate' && (
+              <div className="text-red-500">Passwords do not match</div>
+            )}
+        </div>
+
+        <div className="mb-4">
+          <button className="primary-button">Register</button>
         </div>
         <div className="mb-4">
-          {`Don't have an account? `}
-          <Link href={`/register?redirect=${redirect || '/'}`}>
-            <a className="pl-2 underline">Register</a>
+          {`Already have an account? `}
+          <Link href={`/login?redirect=${redirect || '/'}`}>
+            <a className="pl-2 underline">Login</a>
           </Link>
         </div>
       </form>
